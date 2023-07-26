5f3339194f29ad1fda9772158924b1242bb6b456
Taki <hashimoto19980924@gmail.com>
2023-07-26 16:20:30 +0900

    feat: sign message by issuer

--- src/resolve-did.ts
+++ src/resolve-did.ts
@@ -3,7 +3,7 @@ import { agent } from './veramo/setup.js'
 async function main() {
   const didDocument = await agent.resolveDid({
     //didUrl: 'did:ethr:goerli:0x0361fe5fcf149e7abdb8bcfd624bfc8aa2aa71f90b9dbfc3d9c933caf36fad0d3c'
-    didUrl: 'did:ethr:goerli:0x0361fe5fcf149e7abdb8bcfd624bfc8aa2aa71f90b9dbfc3d9c933caf36fad0d3c#delegate-2'
+    didUrl: 'did:ethr:goerli:0x03c5b0ebe28f368103465e7b1ab01d16bd2ba6e6493f065eb06e246db8c766159b'
   })
   console.log(didDocument.didDocument)
 }
